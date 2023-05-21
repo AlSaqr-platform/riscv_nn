779b107a5968a2a7cb37bd8270b44c10a4187b6e
Yvan Tortorella <ytortorella@lagrev3.ee.ethz.ch>
2023-05-21 11:22:51 +0200

    Updated FPU.

--- rtl/include/riscv_defines.sv
+++ rtl/include/riscv_defines.sv
@@ -452,6 +452,7 @@ parameter bit C_RVD = 1'b0; // Is D extension enabled - NOT SUPPORTED CURRENTLY
 parameter bit C_XF16    = 1'b1; // Is half-precision float extension (Xf16) enabled
 parameter bit C_XF16ALT = 1'b1; // Is alternative half-precision float extension (Xf16alt) enabled
 parameter bit C_XF8     = 1'b1; // Is quarter-precision float extension (Xf8) enabled
+parameter bit C_XF8ALT  = 1'b0; // Is quarter-precision float extension (Xf8alt) enabled
 parameter bit C_XFVEC   = 1'b1; // Is vectorial float extension (Xfvec) enabled
 
 // FPnew configuration
@@ -465,9 +466,11 @@ parameter int unsigned C_LAT_FP32       = 'd1;
 parameter int unsigned C_LAT_FP16       = 'd1;
 parameter int unsigned C_LAT_FP16ALT    = 'd1;
 parameter int unsigned C_LAT_FP8        = 'd1;
+parameter int unsigned C_LAT_FP8ALT     = 'd0;
 parameter int unsigned C_LAT_DIVSQRT    = 'd2; // divsqrt post-processing pipe
 parameter int unsigned C_LAT_CONV       = 'd1;
 parameter int unsigned C_LAT_NONCOMP    = 'd1;
+parameter int unsigned C_LAT_DOTP       = 'd0;
 
 // General FPU-specific defines
 
@@ -477,6 +480,7 @@ parameter C_FLEN = C_RVD     ? 64 : // D ext.
                    C_XF16    ? 16 : // Xf16 ext.
                    C_XF16ALT ? 16 : // Xf16alt ext.
                    C_XF8     ? 8 :  // Xf8 ext.
+                   C_XF8ALT  ? 8 :  // Xf8alt ext.
                    0;               // Unused in case of no FP
 
 parameter C_FFLAG             = 5;
--- rtl/riscv_ex_stage.sv
+++ rtl/riscv_ex_stage.sv
@@ -543,21 +543,23 @@ module riscv_ex_stage
             Width:         C_FLEN,
             EnableVectors: C_XFVEC,
             EnableNanBox:  1'b0,
-            FpFmtMask:     {C_RVF, C_RVD, C_XF16, C_XF8, C_XF16ALT},
-            IntFmtMask:    {C_XFVEC && C_XF8, C_XFVEC && (C_XF16 || C_XF16ALT), 1'b1, 1'b0}
+            FpFmtMask:     {C_RVF, C_RVD, C_XF16, C_XF8, C_XF16ALT, C_XF8ALT},
+            IntFmtMask:    {C_XFVEC && (C_XF8 || C_XF8ALT), C_XFVEC && (C_XF16 || C_XF16ALT), 1'b1, 1'b0}
           };
 
           // Implementation (number of registers etc)
           localparam fpnew_pkg::fpu_implementation_t FPU_IMPLEMENTATION = '{
             PipeRegs:  '{// FP32, FP64, FP16, FP8, FP16alt
-                         '{C_LAT_FP32, C_LAT_FP64, C_LAT_FP16, C_LAT_FP8, C_LAT_FP16ALT}, // ADDMUL
+                         '{C_LAT_FP32, C_LAT_FP64, C_LAT_FP16, C_LAT_FP8, C_LAT_FP16ALT, C_LAT_FP8ALT}, // ADDMUL
                          '{default: C_LAT_DIVSQRT}, // DIVSQRT
                          '{default: C_LAT_NONCOMP}, // NONCOMP
-                         '{default: C_LAT_CONV}},   // CONV
+                         '{default: C_LAT_CONV},   // CONV
+                         '{default: C_LAT_DOTP}}, // SDOTP
             UnitTypes: '{'{default: fpnew_pkg::MERGED}, // ADDMUL
                          '{default: C_DIV},               // DIVSQRT
                          '{default: fpnew_pkg::PARALLEL}, // NONCOMP
-                         '{default: fpnew_pkg::MERGED}},  // CONV
+                         '{default: fpnew_pkg::MERGED},  // CONV
+                         '{default: fpnew_pkg::DISABLED}}, // SDOTP
             PipeConfig: fpnew_pkg::AFTER
           };
 
@@ -572,6 +574,7 @@ module riscv_ex_stage
           ) i_fpnew_bulk (
             .clk_i          ( clk                                   ),
             .rst_ni         ( rst_n                                 ),
+            .hart_id_i      ( '0                                    ),
             .operands_i     ( apu_operands_i                        ),
             .rnd_mode_i     ( fpnew_pkg::roundmode_e'(fp_rnd_mode)  ),
             .op_i           ( fpnew_pkg::operation_e'(fpu_op)       ),
@@ -581,6 +584,7 @@ module riscv_ex_stage
             .int_fmt_i      ( fpnew_pkg::int_format_e'(fpu_int_fmt) ),
             .vectorial_op_i ( fpu_vec_op                            ),
             .tag_i          ( 1'b0                                  ),
+            .simd_mask_i    ( '1                                    ),
             .in_valid_i     ( apu_req                               ),
             .in_ready_o     ( FPU_ready_int                         ),
             .flush_i        ( 1'b0                                  ),
